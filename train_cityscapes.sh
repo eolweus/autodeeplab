a9e308ece4d26b1ecb3a04ebb3887fe15cb7f54a
Erling Olweus <erling.olweus@gmail.com>
2023-05-08 16:56:03 +0000

    feat: update run scripts

--- train_cityscapes.sh
+++ train_cityscapes.sh
@@ -1,3 +1,3 @@
 CUDA_VISIBLE_DEVICES=0 python train_autodeeplab.py \
- --batch-size 2 --dataset cityscapes --checkname firstTest \
+ --batch-size 6 --dataset cityscapes --checkname firstTest \
  --alpha_epoch 20 --filter_multiplier 8 --resize 512 --crop_size 321
--- train_solis.sh
+++ train_solis.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=0 python train_autodeeplab.py \
- --batch-size 6 --dataset solis --checkname no_bb_5010 \
- --alpha_epoch 20 --filter_multiplier 8 --resize 224 --crop_size 224 \
- --num_bands 12 --epochs 40 --num_images 5010
+ --batch-size 4 --dataset solis --checkname no_bb_5008 \
+ --alpha_epoch 5 --filter_multiplier 8 --resize 224 --crop_size 224 \
+ --num_bands 12 --epochs 30 --num_images 5008 --loss-type focal
